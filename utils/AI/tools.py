bdb8e88c3822f6939ebba055c3a0a24dbcb28f43
Orhan Yavuz <oyavuzjr@gmail.com>
2024-06-20 23:13:13 +0300

    finalized schemas

--- utils/AI/tools.py
+++ utils/AI/tools.py
@@ -2,18 +2,24 @@ from typing import List
 from langchain.tools import BaseTool, StructuredTool, tool, Tool
 from langchain.output_parsers import PydanticOutputParser, StructuredOutputParser
 from utils.AI.question_generator import Exercises
-from pydantic.v1 import BaseModel
+from langchain.pydantic_v1 import BaseModel, Field, validator
+
+# parser = PydanticOutputParser(pydantic_object=Exercises)
+# output_parser = StructuredOutputParser.from_response_schemas(Exercises)
+# format_instruction = output_parser.get_format_instructions()
+
+class SentenceCompletionArgsSchema(BaseModel):
+    questions: List[str] = Field(description="List of exercises with blank spots")
+    answers: List[str] = Field(description="List of answers omitted from the blank spots")
+
 
 # parser = PydanticOutputParser(pydantic_object=Exercises)
 # output_parser = StructuredOutputParser.from_response_schemas(Exercises)
 # format_instruction = output_parser.get_format_instructions()
 
 class CreateLectureArgsSchema(BaseModel):
+    topic:str
     lecture:str
-
-class SentenceCompletionArgsSchema(BaseModel):
-    questions:List[str]
-    answers:List[str]
 
 @tool("create_sentence_completion_problems",args_schema=SentenceCompletionArgsSchema)
 def create_sentence_completion_problems(questions:List[str], answers:List[str]):
@@ -22,17 +28,17 @@ def create_sentence_completion_problems(questions:List[str], answers:List[str]):
     Provide questions where the blank part is '___'
     And provide answers that fill in the blank.
     """
-    print({'questions': questions, 'answers': answers})
+    return({'questions': questions, 'answers': answers})
     return {"success":True}
 
 
 @tool("create_lecture",args_schema=CreateLectureArgsSchema)
-def create_lecture(lecture:str):
+def create_lecture(topic:str,lecture:str):
     """
     Creates a lecture in the topic that the student has requested.
     """
-    print(lecture)
-    return {"success":True}
+    # return {"success":True}
+    return {"topic":topic,"lecture":lecture}
 
 
 
